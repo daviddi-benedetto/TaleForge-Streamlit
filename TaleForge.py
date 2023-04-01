5151cf62d6535dce422242cda8e1c981a86f0377
David Di-Benedetto <110741357+daviddi-benedetto@users.noreply.github.com>
2023-03-31 23:30:59 -0500

    Update TaleForge.py

--- TaleForge.py
+++ TaleForge.py
@@ -1,3 +1,7 @@
+### TaleForge ###
+# Made by David Di-Benedetto
+
+### Imports ---------------------------------------------------------------
 import streamlit as st
 import os
 import openai
@@ -17,63 +21,69 @@ api_key = "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"
 # Initialize the OpenAI API client
 openai.api_key = api_key
 
-def get_unique_themes():
-    return [
-        "fantasy",
-        "sci-fi",
-        "horror",
-        "mystery",
-        "historical fiction",
-        "superhero",
-        "dystopian",
-        "post-apocalyptic",
-        "cyberpunk",
-        "steampunk",
-        "paranormal romance",
-        "crime noir",
-        "zombie apocalypse",
-        "pirate adventure",
-        "space opera",
-    ]
 
+### Splash Page Interactions -----------------------------------------------
+# Your Name
 def get_character_name():
-    character_name = st.text_input("Enter your character's name:")
+    character_name = st.text_input("🧙‍♂️ Enter your character's name:")
     return character_name
 
-# Function to call the ChatGPT API
-def generate_response(user_choice, conversation_history):
-    progress_placeholder = st.empty()
-    progress_bar = progress_placeholder.progress(0)
-    message_placeholder = st.empty()
+# Choose a Role
+def get_unique_story_types():
+    return [
+        "Random 🎲", "Hero 🦸‍♂️🦸‍♀️", "Mentor 🧑‍🏫", "Sidekick 🐶", "Sage 🧙‍♂️🧙‍♀️", 
+        "Innocent 😇", "Explorer 🌍", "Rebel 🤘", "Jester 🤡", "Lover ❤️", 
+        "Caregiver 🤱", "Creator 🎨", "Destroyer 💥", "Magician 🎩", "Ruler 👑", 
+        "Trickster 🃏", "Outlaw 🏴‍☠️", "Scapegoat 🐐", "Villain 😈", "Anti-Hero 🦹‍♂️🦹‍♀️", 
+        "Warrior ⚔️", "Thief 🦝", "Femme Fatale 💄", "Damsel in Distress 👧", "Wise Old Man/Woman 👴👵", 
+        "Mad Scientist 🧪", "Detective 🕵️‍♂️🕵️‍♀️", "Everyman/Everywoman 👨‍👩‍👧‍👦", "Con Artist 🕵️‍♂️", "Lone Wolf 🐺", 
+        "Orphan 😔", "Sinner 😈", "Saint 😇", "Coward 🐔", "Martyr 🙏", 
+        "Clown 🤡", "Snob 🧐", "Charmer 😍", "Good Samaritan 🙏", "Seducer 😘", 
+        "Boss 👔", "Henchman 👥", "Revolutionary ✊", "Priest ⛪", "Soldier 🎖️", 
+        "Wanderer 🚶", "Survivor 🌟", "Loner 🧍", "Misfit 🤪", "Addict 💉", 
+        "Visionary 🌟", "Nurturer 🤱", "Enigma ❓", "Temptress 😈", "Crusader 🏰", 
+        "Artist 🎨", "Idealist 💭", "Realist 👀", "Analyst 🧐", "Entrepreneur 💼", 
+        "Opportunist 💰", "Escapist 🌟", "Prophet 📖", "Healer 🧑‍⚕️", "Protector 🛡️", 
+        "Avenger 🔪", "Trickster Hero 🦊", "Savior 🙏", "Redeemer 🙌", "Guardian 🛡️", 
+        "Liberator 🌟", "Dreamer 💭", "Loyalist 🤝", "Vengeful 🔪", "Leader 👑", 
+        "Innovator 🔬", "Empath 🤝", "Listener 👂", "Mentor's Shadow 👤", "Seeker 🔍", 
+        "Neutral 🤷", "Reluctant Hero 😕", "Shadow 👥", "Adversary 😠", "Monster 👹", 
+        "Survivor 🌟", "Outcast 🚪", "Dark Lord 🦹"]
 
-    # Animate "Generating your story..." message
-    for i in range(3):
-        message_placeholder.text("Generating your story" + "." * (i % 4))
-        time.sleep(0.5)
 
-    prompt = f"{conversation_history}User chose option {user_choice}. Generate a cohesive and engaging plot that keeps the user intrigued. Second person. Create dramatic situations and make it possible for the user to die if they choose the wrong choice, thus ending the game early. Provide 2 paragraphs of continuation of the story and 4 new choices for the user's next action. Please ensure each choice is on a new line.\n\n"
+# Choose a Theme
+def get_unique_themes():
+    return ["Random 🎲", "Fantasy 🧙‍♂️", "Sci-Fi 🚀", "Horror 🧟‍♂️", "Mystery 🕵️‍♂️", "Historical Fiction 🏰", "Superhero 🦸‍♂️",
+            "Dystopian 🌆", "Post-Apocalyptic 🌍", "Cyberpunk 🤖", "Steampunk ⚙️", "Paranormal Romance 👻",
+            "Crime Noir 🕵️‍♂️", "Zombie Apocalypse 🧟‍♂️", "Pirate Adventure 🏴‍☠️", "Space Opera 🚀",
+            "Time Travel ⏰", "Alternate History 🕰️", "Survival 🏞️", "War ⚔️", "Comedy 🤣",
+            "Action/Adventure ⚔️", "Romantic Comedy ❤️🤣", "Gothic 🦇", "Fairy Tale 🧚", 
+            "Epic Fantasy 🐉", "Apocalyptic 🌍", "Alien Invasion 👽", "Artificial Intelligence 🤖",
+            "Drama 🎭", "Mythology 🏛️", "Urban Fantasy 🏙️", "Historical Fantasy 🏰", "Space Western 🤠",
+            "Military Science Fiction 🎖️", "Soft Science Fiction 🚀", "Hard Science Fiction 🚀", 
+            "Contemporary Fantasy 🏙️", "Dark Fantasy 🖤", "Sword and Sorcery ⚔️", "Stealth Action 🕵️‍♂️",
+            "Spy Fiction 🕵️‍♂️", "Sports Fiction 🏀", "Speculative Fiction 🤔", "Alternate Universe 🌎",
+            "Erotic Romance 🔥", "New Weird 🤪", "Drama-Romance 🎭❤️", "Historical Romance 💕",
+            "Young Adult 🧑‍🎓", "Comedy-Drama 🤣🎭", "Slice of Life 🌳", "Psychological Thriller 🧠🕵️‍♂️",
+            "Political Thriller 🏛️🕵️‍♂️", "Coming-of-Age 🧑‍🎓", "Teen Romance 💕🧑‍🎓", "Teen Comedy 🤣🧑‍🎓",
+            "Medical Drama 🏥", "Disaster Movie 🌪️", "Survival Horror 🧟‍♂️🌳", "Creature Feature 🐲",
+            "Military Thriller 🎖️🕵️‍♂️", "Legal Drama ⚖️", "Courtroom Thriller ⚖️🕵️‍♂️", "Heist 💰",
+            "Gangster 🕵️‍♂️", "Psychological Horror 🧠👻", "Supernatural Thriller 👻🕵️‍♂️",
+            "Political Satire 🏛️🤣", "Existential Drama 🧐🎭", "Surrealism 🤪", "Postmodernism 🕰️🤪",
+            "Magical Realism", "Absurdist Comedy 🤪🤣", "Documentary-style 📷", "Found Footage 📹", "Mockumentary 🤣📹",
+            "Musical Comedy 🎶🤣", "Science Fiction Horror 🧟‍♂️🚀", "Steampunk Romance 💕⚙️",
+            "Slasher 🔪", "Monster Movie 🐲", "Disaster Thriller 🌪️🕵️‍♂️", "Alien Abduction 👽",
+            "Robotics and AI 🤖", "Space Horror 👽🚀", "Cyber-Thriller 🤖🕵️‍♂️", "Military Science Fantasy 🎖️🐉",
+            "Techno-Thriller 🕵️‍♂️🤖", "Psychological Drama 🧠🎭", "Eco-Thriller 🌿🕵️‍♂️", "Crime Drama 🕵️‍♂️🎭",
+            "Psychological Mystery 🧠🕵️‍♂️", "Historical Epic 🏰🎖️", "War Drama 🎖️🎭", "Postcolonial 🌍🕰️",
+            "Martial Arts 🥋", "Underwater Adventure 🏊‍♂️🐠", "Time Loop ⏰", "Mystery Comedy 🕵️‍♂️🤣"
+]
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=3000,
-        n=1,
-        stop=None,
-        temperature=1,
-    )
 
-    progress_bar.progress(100)
-    time.sleep(0.5)
-    message_placeholder.text("Story generated!")
-    time.sleep(0.5)
 
-    story, choices = response.choices[0].text.strip().split('\n\n', 1)
 
-    progress_placeholder.empty()
-    message_placeholder.empty()
 
-    return story, choices
-
+### Initial Scenario Generation --------------------------------------------
 
 # Function to generate the initial setting, situation, and character
 def generate_initial_scenario():
@@ -82,14 +92,15 @@ def generate_initial_scenario():
     message_placeholder = st.empty()
     message_placeholder.text("Generating initial scenario...")
 
-    # Incorporate the selected genre into the initial prompt
+    # Incorporate the selected genre and character name into the initial prompt
     genre = st.session_state.theme
-    initial_prompt = f"Create a connected setting, situation, and character in a {genre} land. The user is the character. Second person. Describe it in 2 or 3 paragraphs and give 4 choices in a list for user's next action."
+    story_type = st.session_state.story_type
+    initial_prompt = f"Story Genre and Setting:{genre}, Character Role:{story_type} Create the introduction to a long and elaborate CYOA-style story IN SECOND PERSON with a minimum of 3 paragraphs in a connected setting and creative situation. The character's name is {st.session_state.character_name}. Give the world a unique name, history, setting, and describe it to the player. Give CYOA-like open ended suggestions for the user without telling them the outcomes of their actions."
 
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=initial_prompt,
-        max_tokens=3000,
+        max_tokens=3500,
         n=1,
         stop=None,
         temperature=1,
@@ -101,37 +112,117 @@ def generate_initial_scenario():
     time.sleep(0.5)
 
     initial_scenario = response.choices[0].text.strip()
-    story, choices = initial_scenario.split('\n\n', 1)
+
+    # Replace the placeholder name in the story with the actual character name
+    story = initial_scenario.replace("Player", st.session_state.character_name)
+
+    split_scenario = story.split('\n\n', 1)
+
+    if len(split_scenario) == 2:
+        story, choices = split_scenario
+    else:
+        story = split_scenario[0]
+        choices = "No choices were generated. Please try again."
 
     progress_placeholder.empty()
     message_placeholder.empty()
 
     return story, choices
 
+### Continued Story Generation ---------------------------------------------
+
+# Function to generate the continued story
+def generate_response(user_choice, conversation_history):
+    progress_placeholder = st.empty()
+    progress_bar = progress_placeholder.progress(0)
+    message_placeholder = st.empty()
+
+    # Animate "Generating your story..." message
+    for i in range(3):
+        message_placeholder.text("Generating your story" + "." * (i % 4))
+        time.sleep(0.5)
+
+    prompt = f"{conversation_history}\n- User's action: {user_choice} \n- Task: Repeat to the user what action they took in a story-book way. Make a spacer line. Then, generate a cohesive and engaging plot as a direct continuation from the action the user took that keeps the user intrigued. Genre:{st.session_state.theme}, Character role:{st.session_state.story_type}. Tell the story in second person, with the player being the one addressed. Create a well-written plot and make it possible for the user to die if they choose the wrong action, thus ending the game early. Provide 2 or 3 paragraphs of direct continuation of the story. Give CYOA-like open ended suggestions for the user without telling them the outcomes of their actions at the end."
+
+    # Keep making API calls until choices are generated
+    choices_generated = False
+    while not choices_generated:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=3000,
+            n=1,
+            stop=None,
+            temperature=1,
+        )
+
+        split_response = response.choices[0].text.strip().split('\n\n', 1)
+
+        if len(split_response) == 2:
+            story, choices = split_response
+            choices_generated = True
+        else:
+            story = split_response[0]
+            choices = "No choices were generated. Please try again."
+
+    progress_bar.progress(100)
+    time.sleep(0.5)
+    message_placeholder.text("Story generated!")
+    time.sleep(0.5)
+
+    progress_placeholder.empty()
+    message_placeholder.empty()
+
+    st.session_state.user_action = "" # Clear the user input box
+
+    return story, choices
+
+
+# Get user action
+def get_user_action():
+    if "clear_input" in st.session_state and st.session_state.clear_input:
+        user_action = st.text_input("Enter your action 👉", value="")
+        st.session_state.clear_input = False
+    else:
+        user_action = st.text_input("Enter your action 👉")
+    return user_action
+
+
+
 
 # Function to condense conversation history
 def condense_history(history):
     condensed_history = []
-   
+
     for text in history:
         # Only remove leading and trailing whitespace
         text = text.strip()
         condensed_history.append(text)
     return "\n\n".join(condensed_history[::-1])
 
+### Splash Page Creation ---------------------------------------------------
+
 # Initialize session state if not exists
 if "on_splash_page" not in st.session_state:
     st.session_state.on_splash_page = True
+    st.session_state.conversation_history = []
+
+if "input_box_value" not in st.session_state:
+    st.session_state.input_box_value = ""
+
 
 # Splash page
 if st.session_state.on_splash_page:
     st.markdown("<h1 style='text-align: center;'>⚒️ Welcome to TaleForge 📚</h1>", unsafe_allow_html=True)
     st.markdown("""
-        <p style='text-align: center;'>Welcome to TaleForge, the interactive storytelling experience where you get to create your own adventure and craft a unique story that has never been made before. With the help of OpenAI's language model, your choices will lead you down a path full of surprises and adventure. Let's get started and see where your journey takes you! 🌟</p>
+        <p style='text-align: center;'>Embark on an infinite adventure of free will with TaleForge! Create your own unique story with OpenAI's latest language models, and experience a world full of surprises and excitement :)</p>
     """, unsafe_allow_html=True)
 
+
+
     st.session_state.character_name = get_character_name()
-    st.session_state.theme = st.selectbox("Select a theme for your adventure:", get_unique_themes())
+    st.session_state.story_type = st.selectbox("🎭 Select a role:", get_unique_story_types())
+    st.session_state.theme = st.selectbox("🎨 Select a theme for your adventure:", get_unique_themes())
 
     if st.session_state.character_name and st.button("Begin Adventure"):
         st.session_state.on_splash_page = False
@@ -140,7 +231,14 @@ if st.session_state.on_splash_page:
         st.session_state.choices = initial_choices
         st.experimental_rerun()
 
+    st.write("")
+    st.markdown("""
+        <p style='text-align: center;'>Made by <a href='https://daviddi-benedetto.com' target='_blank'>David Di-Benedetto</a></p>
+    """, unsafe_allow_html=True)
 
+
+
+### Overview ---------------------------------------------------------------
 if not st.session_state.on_splash_page:
     # Streamlit App
 
@@ -154,77 +252,42 @@ if not st.session_state.on_splash_page:
 
     with tabs[0]:
         # Display the latest response
-        st.subheader(f"{st.session_state.character_name}'s Story Unfolds...")
+        st.subheader(f"{st.session_state.character_name}'s Tale")
         st.write(st.session_state.conversation_history[0])
 
-        st.write("")  # Add some spacing
 
         # Display choices
         st.write(st.session_state.choices)
 
-        st.write("")  # Add some spacing
 
-        # Emoji buttons
-        col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
-        button_1 = col1.button(" 1️⃣ ", key='button1', help="Choose option 1")
-        button_2 = col2.button(" 2️⃣ ", key='button2', help="Choose option 2")
-        button_3 = col3.button(" 3️⃣ ", key='button3', help="Choose option 3")
-        button_4 = col4.button(" 4️⃣ ", key='button4', help="Choose option 4")
-        reset_button = col5.button(" 🔄 ", key='reset_button', help="Reset the story")
-        quit_button = col6.button(" 🎬 ", key='quit_button', help="End the story")
+        # Get user action
+        st.session_state.user_action = get_user_action()
 
-        st.write("")  # Add some spacing
+        # Generate the story based on user action
+        if st.session_state.user_action:
+            if st.button("Submit Action"):
+                new_story, new_choices = generate_response(st.session_state.user_action, condense_history(st.session_state.conversation_history))
+                st.session_state.conversation_history.insert(0, new_story)
+                st.session_state.choices = new_choices
+                st.session_state.input_box_value = ""  # Clear the input box value
+                st.experimental_rerun()
 
-        # Custom location for the progress bar
-        progress_col, _ = st.columns([1, 3])
-        progress_placeholder = progress_col.empty()
 
-    # Handle button actions
-    user_choice = None
-    if button_1:
-        user_choice = 1
-    elif button_2:
-        user_choice = 2
-    elif button_3:
-        user_choice = 3
-    elif button_4:
-        user_choice = 4
-
-    if user_choice:
-        # Generate a response
-        condensed_history = condense_history(st.session_state.conversation_history)
-        response_story, response_choices = generate_response(user_choice, condensed_history)
-
-        # Update the conversation history and choices
-        st.session_state.conversation_history.insert(0, response_story)
-        st.session_state.choices = response_choices
-
-        # Rerun to display the latest response
-        st.experimental_rerun()
-
-    if reset_button:
-        st.session_state.conversation_history = []
-        st.session_state.on_splash_page = True
-        st.experimental_rerun()
-
-    if quit_button:
-        st.write(f"Thank you for playing {st.session_state.character_name}! Your story has come to an end. 🎬")
-        st.balloons()
-        st.stop()
 
     with tabs[1]:
-        st.subheader("🔍 Conversation History")
-        for i, history_item in enumerate(st.session_state.conversation_history[::-1]):
-            st.write(f"Step {i+1}:")
-            st.write(history_item)
-            st.write("")  # Add some spacing
+        st.subheader("📜 Your Story")
+        for history_item in reversed(st.session_state.conversation_history):
+            for item in history_item.split('\n\n'):
+                st.write("➔", item)
+            st.write(st.session_state.choices)  # display choices for each step
 
     with tabs[2]:
         st.subheader("🎉 About TaleForge")
         st.markdown("""
             Hi there, I'm David Di-Benedetto, the creator of TaleForge! I'm passionate about bringing ideas to life and designing interactive experiences that captivate and entertain. With the power of OpenAI's GPT-3, I've created this web app to provide you with a unique, immersive adventure where your choices shape the narrative.
-           
+        
             When I'm not busy working on TaleForge, I love exploring new ideas and creating amazing projects that spark curiosity and inspiration. I invite you to check out my website at [daviddi-benedetto.com](https://daviddi-benedetto.com) to see what else I've been up to. You'll find a collection of my work ranging from creative instruments to aerospace, and much more.
-           
+        
             I hope you enjoy your time in TaleForge, and I'd love to hear your thoughts or feedback (daviddi-benedetto@outlook.com). Happy adventuring!
         """)
+
