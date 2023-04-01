e0ac50b3bea730b7ac429c4e8d429019d912b0e4
David Di-Benedetto <110741357+daviddi-benedetto@users.noreply.github.com>
2023-04-01 14:37:32 -0500

    Update TaleForge.py

--- TaleForge.py
+++ TaleForge.py
@@ -3,11 +3,17 @@
 
 ### Imports ---------------------------------------------------------------
 import streamlit as st
-import os
 import openai
-import re
 import time
-import random
+from io import BytesIO
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+import base64
+
 
 # Set page config before other imports
 wide_mode = False  # Set to True to enable wide mode
@@ -78,10 +84,6 @@ def get_unique_themes():
             "Psychological Mystery 🧠🕵️‍♂️", "Historical Epic 🏰🎖️", "War Drama 🎖️🎭", "Postcolonial 🌍🕰️",
             "Martial Arts 🥋", "Underwater Adventure 🏊‍♂️🐠", "Time Loop ⏰", "Mystery Comedy 🕵️‍♂️🤣"
 ]
-
-
-
-
 
 ### Initial Scenario Generation --------------------------------------------
 
@@ -177,16 +179,25 @@ def generate_response(user_choice, conversation_history):
 
     return story, choices
 
-
 # Get user action
 def get_user_action():
+    col1, col2 = st.columns([4, 1])  # Create two columns with a 4:1 ratio
+
     if "clear_input" in st.session_state and st.session_state.clear_input:
-        user_action = st.text_input("Enter your action 👉", value="")
+        user_action = col1.text_input("Enter your action 👉", value="")
         st.session_state.clear_input = False
     else:
-        user_action = st.text_input("Enter your action 👉")
-    return user_action
+        user_action = col1.text_input("Enter your action 👉")
 
+    submit_button = col2.button("Submit")  # Add the submit button in the second column
+
+    if user_action != st.session_state.input_box_value:
+        st.session_state.input_box_value = user_action
+        st.session_state.new_input_entered = submit_button  # Update this line
+    else:
+        st.session_state.new_input_entered = False
+
+    return user_action
 
 
 
@@ -200,6 +211,42 @@ def condense_history(history):
         condensed_history.append(text)
     return "\n\n".join(condensed_history[::-1])
 
+### PDF Generation --------------------------------------------------------
+def generate_pdf(history):
+    buffer = BytesIO()
+    doc = SimpleDocTemplate(buffer, pagesize=letter)
+    styles = getSampleStyleSheet()
+    story = []
+
+    # Add a title
+    title_style = ParagraphStyle(
+        name="Title",
+        parent=styles["Heading1"],
+        fontName="Helvetica",
+        fontSize=24,
+        textColor=colors.HexColor('#3f3f3f'),
+        spaceAfter=12
+    )
+    story.append(Paragraph("Your TaleForge Story", title_style))
+    story.append(Spacer(1, 12))
+
+    # Add the story content
+    content_style = ParagraphStyle(
+        name="Content",
+        parent=styles["BodyText"],
+        fontName="Helvetica",
+        fontSize=12,
+        textColor=colors.HexColor('#3f3f3f'),
+        spaceAfter=12
+    )
+
+    for line in history:
+        story.append(Paragraph(line, content_style))
+
+    doc.build(story)
+    buffer.seek(0)
+    return buffer
+
 ### Splash Page Creation ---------------------------------------------------
 
 # Initialize session state if not exists
@@ -209,7 +256,6 @@ if "on_splash_page" not in st.session_state:
 
 if "input_box_value" not in st.session_state:
     st.session_state.input_box_value = ""
-
 
 # Splash page
 if st.session_state.on_splash_page:
@@ -236,9 +282,7 @@ if st.session_state.on_splash_page:
         <p style='text-align: center;'>Made by <a href='https://daviddi-benedetto.com' target='_blank'>David Di-Benedetto</a></p>
     """, unsafe_allow_html=True)
 
-
-
-### Overview ---------------------------------------------------------------
+### Tabs ---------------------------------------------------------------
 if not st.session_state.on_splash_page:
     # Streamlit App
 
@@ -263,19 +307,40 @@ if not st.session_state.on_splash_page:
         # Get user action
         st.session_state.user_action = get_user_action()
 
+        # Check if the story_generated flag exists, if not, initialize it
+        if "story_generated" not in st.session_state:
+            st.session_state.story_generated = False
+
         # Generate the story based on user action
-        if st.session_state.user_action:
-            if st.button("Submit Action"):
-                new_story, new_choices = generate_response(st.session_state.user_action, condense_history(st.session_state.conversation_history))
-                st.session_state.conversation_history.insert(0, new_story)
-                st.session_state.choices = new_choices
-                st.session_state.input_box_value = ""  # Clear the input box value
-                st.experimental_rerun()
+        if st.session_state.new_input_entered and not st.session_state.story_generated:
+            new_story, new_choices = generate_response(st.session_state.user_action, condense_history(st.session_state.conversation_history))
+            st.session_state.conversation_history.insert(0, new_story)
+            st.session_state.choices = new_choices
+            st.session_state.story_generated = True  # Set the story_generated flag to True
+            st.experimental_rerun()
 
-
+        # Reset the story_generated flag and new_input_entered flag when the user inputs a new action
+        if st.session_state.new_input_entered and st.session_state.story_generated:
+            st.session_state.story_generated = False
+            st.session_state.new_input_entered = False
+            st.experimental_rerun()
 
     with tabs[1]:
         st.subheader("📜 Your Story")
+
+        # Add a download button for the PDF
+        if st.button("Download History as PDF"):
+            history_lines = []
+            for history_item in reversed(st.session_state.conversation_history):
+                for item in history_item.split('\n\n'):
+                    history_lines.append("➔ " + item)
+
+            pdf_buffer = generate_pdf(history_lines)
+            b64 = base64.b64encode(pdf_buffer.getvalue()).decode()
+            href = f'<a href="data:application/octet-stream;base64,{b64}" download="story_history.pdf">Click here to download your story history as a PDF</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
+        # Display the story history
         for history_item in reversed(st.session_state.conversation_history):
             for item in history_item.split('\n\n'):
                 st.write("➔", item)
@@ -291,3 +356,11 @@ if not st.session_state.on_splash_page:
             I hope you enjoy your time in TaleForge, and I'd love to hear your thoughts or feedback (daviddi-benedetto@outlook.com). Happy adventuring!
         """)
 
+### Hide Streamlit elements ---------------------------------------------------
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
